96dada6ee8f9a889fa52c70d9bb0a46dde405768
super winners <advayvk@gmail.com>
2026-06-10 23:54:32 -0500

    Initial Commit

---
+++ zenith/zenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenithApp.swift
+//  zenith
+//
+//  Created by Advay Kongorpalli on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct zenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
